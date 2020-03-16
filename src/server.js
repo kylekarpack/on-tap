c637e0547510aaa47fa82ebd326d04644d1a1b25
Kyle Karpack <kylekarpack@gmail.com>
2020-03-15 19:32:20 -0700

    Basic UI added

--- src/server.js
+++ src/server.js
@@ -1,18 +1,27 @@
 
-const express = require("express")
+const express = require("express");
+const Flatstick = require("./connectors/flatstick");
+
 const app = express()
 const port = process.env.PORT || 3000;
 
 class Server {
 
+	async get(Client, res) {
+		const client = new Client();
+		let data = await client.execute();
+		res.json(data);
+	}
+
 	init() {
 		app.get('/', (req, res) => {
-			res.send('Hello World')
+			res.sendFile(__dirname + '/templates/index.html');
 		});
 
-		app.get("/test", (req, res) => {
-			res.json({ test: 1 })
+		app.get("/flatstick", async (req, res) => {
+			this.get(Flatstick, res);
 		})
+
 		app.listen(port);
 	}
 }
