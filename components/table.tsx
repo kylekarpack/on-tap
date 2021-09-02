b5b1d77030caf24e4e91dac6abcd8c62702d8c0c
Kyle Karpack <kylekarpack@gmail.com>
2021-09-01 19:55:10 -0700

    Update rsuite

--- components/table.tsx
+++ components/table.tsx
@@ -1,18 +1,15 @@
 import { useQuery } from "@apollo/client";
 import Image from "next/image";
 import React, { useState } from "react";
-import Ratings from "react-ratings-declarative";
-import { Col, Grid, Loader, Panel, PanelGroup, Row, Table } from "rsuite";
+import { Col, Grid, Loader, Panel, PanelGroup, Row } from "rsuite";
 import { GET_BEERS } from "util/queries/getBeers";
 import { Beer } from "util/types/beer";
 import { sortTable } from "util/utils";
 
-const { Column, HeaderCell, Cell } = Table;
-
 const cardStyle = {
   justifyContent: "center",
   alignItems: "center",
-	marginTop: "1em"
+  marginTop: "1em"
 };
 
 const slimText = {
@@ -118,88 +115,5 @@ export default function BeerTable({ venue }) {
         </Panel>
       ))}
     </PanelGroup>
-  );
-
-  return error ? (
-    <div>Error: {error.message}</div>
-  ) : (
-    <Table
-      height={550}
-      loading={loading}
-      data={sortTable(state, data?.beers)}
-      sortColumn={state.sortColumn}
-      sortType={state.sortType}
-      onSortColumn={handleSort}
-      affixHeader
-      affixHorizontalScrollbar
-    >
-      <Column flexGrow={1} minWidth={75} fixed sortable>
-        <HeaderCell>Rating</HeaderCell>
-        <Cell dataKey="rating" f>
-          {(rowData: Beer) => (
-            <>
-              {rowData.rating ? (
-                <div style={{ textAlign: "center" }}>
-                  <small style={{ display: "block" }}>{rowData.rating}</small>
-                  <div style={{ marginTop: "-8px" }}>
-                    <Ratings rating={rowData.rating}>
-                      <Ratings.Widget widgetDimension="10px" widgetSpacing="0" />
-                      <Ratings.Widget widgetDimension="10px" widgetSpacing="0" />
-                      <Ratings.Widget widgetDimension="10px" widgetSpacing="0" />
-                      <Ratings.Widget widgetDimension="10px" widgetSpacing="0" />
-                      <Ratings.Widget widgetDimension="10px" widgetSpacing="0" />
-                    </Ratings>
-                  </div>
-                </div>
-              ) : null}
-            </>
-          )}
-        </Cell>
-      </Column>
-      <Column flexGrow={2} minWidth={100} fixed sortable>
-        <HeaderCell>Beer</HeaderCell>
-        <Cell dataKey="beer">
-          {(rowData: Beer) =>
-            rowData.id ? (
-              <a href={`https://untappd.com/beer/${rowData.id}`} target="_blank" rel="nofollow noreferrer">
-                {rowData.beer}
-              </a>
-            ) : (
-              rowData.beer
-            )
-          }
-        </Cell>
-      </Column>
-
-      <Column flexGrow={2} minWidth={200} sortable>
-        <HeaderCell>Brewery</HeaderCell>
-        <Cell dataKey="brewery" />
-      </Column>
-
-      <Column flexGrow={1} minWidth={100} sortable>
-        <HeaderCell>Location</HeaderCell>
-        <Cell dataKey="location" />
-      </Column>
-
-      <Column flexGrow={1} minWidth={100} sortable>
-        <HeaderCell>Style</HeaderCell>
-        <Cell dataKey="style" />
-      </Column>
-
-      <Column flexGrow={1} minWidth={75} sortable>
-        <HeaderCell>ABV</HeaderCell>
-        <Cell dataKey="abv">{(rowData: Beer) => rowData.abv && `${rowData.abv}%`}</Cell>
-      </Column>
-
-      <Column flexGrow={1} minWidth={75} sortable>
-        <HeaderCell>IBU</HeaderCell>
-        <Cell dataKey="ibu" />
-      </Column>
-
-      <Column flexGrow={1} minWidth={75} sortable>
-        <HeaderCell>Remaining</HeaderCell>
-        <Cell dataKey="amount">{(rowData: Beer) => rowData.amount && `${rowData.amount}%`}</Cell>
-      </Column>
-    </Table>
   );
 }
--- pages/api/graphql/connectors/connectorBase.ts
+++ pages/api/graphql/connectors/connectorBase.ts
@@ -1,42 +1,42 @@
-import xray from "x-ray";
 import { Beer } from "util/types/beer";
+import xray from "x-ray";
 import UntappdApi from "./untappd";
 
 class ConnectorBase {
-	protected xray = xray();
-	protected untapped = new UntappdApi();
+  protected xray = xray();
+  protected untapped = new UntappdApi();
 
-	protected url: string;
-	protected selector: string;
-	protected selectors: { [key: string]: string }[];
-	protected key: string;
+  protected url: string;
+  protected selector: string;
+  protected selectors: { [key: string]: string }[];
+  protected key: string;
 
-	public async execute(): Promise<Beer[]> {
-		const pageData = await this.read();
-		const beers = this.process(pageData);
-		return await this.compare(beers);
-	}
+  public async execute(): Promise<Beer[]> {
+    const pageData = await this.read();
+    const beers = this.process(pageData);
+    return await this.compare(beers);
+  }
 
-	protected async read(): Promise<Beer[]> {
-		return this.xray(this.url, this.selector, this.selectors);
-	}
+  protected async read(): Promise<Beer[]> {
+    return this.xray(this.url, this.selector, this.selectors);
+  }
 
-	protected process(data: any[]): any[] {
-		return data;
-	}
+  protected process(data: any[]): any[] {
+    return data;
+  }
 
-	private async compare(beers: Beer[]): Promise<Beer[]> {
-		const output = await Promise.all(
-			beers.map(async (beerData) => {
-				let appendix = await this.untapped.getBeer(beerData);
-				const beer = new Beer(beerData);
-				beer.augment(appendix);
-				return beer;
-			})
-		);
+  private async compare(beers: Beer[]): Promise<Beer[]> {
+    const output = await Promise.all(
+      beers.map(async (beerData) => {
+        let appendix = await this.untapped.getBeer(beerData);
+        const beer = new Beer(beerData);
+        beer.augment(appendix);
+        return beer;
+      })
+    );
 
-		return output;
-	}
+    return output;
+  }
 }
 
 export default ConnectorBase;
