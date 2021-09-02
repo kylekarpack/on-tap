cf70bc4ed6d568da0d60f22afaca8d268c0be9e3
Kyle Karpack <kylekarpack@gmail.com>
2021-09-01 20:54:16 -0700

    Add sorting

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,10 +6,39 @@ import { useState } from "react";
 import { Container, Content, SelectPicker } from "rsuite";
 import { Sort } from "util/types/sort";
 
+const venues = [
+	{ label: "Flatstick Pioneer Square", value: "flatstick" },
+	{ label: "Flatstick Kirkland", value: "flatstickKirkland" },
+	{ label: "Chuck's Greenwood", value: "chucks" },
+];
+
+const sorts: Sort[] = [
+	{
+		label: "Rating (High to Low)",
+		field: "rating",
+		dir: "desc"
+	},
+	{
+		label: "Rating (Low to High)",
+		field: "rating",
+		dir: "asc"
+	},
+	{
+		label: "Name (A to Z)",
+		field: "beer",
+		dir: "asc"
+	},
+	{
+		label: "Name (Z to A)",
+		field: "beer",
+		dir: "desc"
+	}
+]
+
 export default function Home({ initialVenue }: { initialVenue: string }) {
 	const router = useRouter();
 	const [venue, setVenue] = useState<string>(initialVenue || "flatstick");
-	const [sort, setSort] = useState<Sort>({ field: "rating", dir: "desc" });
+	const [sort, setSort] = useState<Sort>(sorts[0]);
 
 	const changeVenue = (venue: string): void => {
 		router.push({
@@ -18,9 +47,9 @@ export default function Home({ initialVenue }: { initialVenue: string }) {
 		setVenue(venue);
 	};
 
-	const changeSort = (sort: Sort): void =>{
-
-	}
+	const changeSort = (_: never, sort: Sort): void =>{
+		setSort(sort);
+	};
 
 	return (
 		<Container>
@@ -35,23 +64,17 @@ export default function Home({ initialVenue }: { initialVenue: string }) {
 					value={venue}
 					searchable={false}
 					cleanable={false}
-					data={[
-						{ label: "Flatstick Pioneer Square", value: "flatstick" },
-						{ label: "Flatstick Kirkland", value: "flatstickKirkland" },
-						{ label: "Chuck's Greenwood", value: "chucks" },
-					]}
+					data={venues}
 				/>
-				{/* <SelectPicker
-					onChange={changeVenue}
-					value={venue}
+				&nbsp;
+				<SelectPicker
+					onSelect={changeSort}
+					value={sort.label}
+					valueKey="label"
 					searchable={false}
 					cleanable={false}
-					data={[
-						{ label: "Flatstick Pioneer Square", value: "flatstick" },
-						{ label: "Flatstick Kirkland", value: "flatstickKirkland" },
-						{ label: "Chuck's Greenwood", value: "chucks" },
-					]}
-				/> */}
+					data={sorts}
+				/>
 				<Table venue={venue} sort={sort} />
 			</Content>
 		</Container>
--- util/types/sort.ts
+++ util/types/sort.ts
@@ -1,6 +1,7 @@
 import { Beer } from "./beer";
 
 export type Sort = {
+	label?: string;
   field: keyof Beer;
   dir: "asc" | "desc";
 };
