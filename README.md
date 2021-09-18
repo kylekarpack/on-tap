59ed7547574e082cde4839ece88b18878490c20d
Kyle Karpack <kylekarpack@gmail.com>
2021-09-18 10:56:34 -0700

    Switch styling to CSS modules

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # on-tap
 
+[![Maintainability Rating](https://sonarcloud.io/api/project_badges/measure?project=kylekarpack_on-tap&metric=sqale_rating)](https://sonarcloud.io/dashboard?id=kylekarpack_on-tap) [![Bugs](https://sonarcloud.io/api/project_badges/measure?project=kylekarpack_on-tap&metric=bugs)](https://sonarcloud.io/dashboard?id=kylekarpack_on-tap)
+
 See what's on tap at local venues around Seattle and sort by various fields including ratings.
 
 ## Development
--- components/list.tsx
+++ components/list.tsx
@@ -6,30 +6,7 @@ import { GET_BEERS } from "util/queries/getBeers";
 import { Beer } from "util/types/beer";
 import { Sort } from "util/types/sort";
 import { sortTable } from "util/utils";
-
-const cardStyle = {
-  justifyContent: "center",
-  alignItems: "center",
-  marginTop: "1em"
-};
-
-const slimText = {
-  fontSize: "0.9em",
-  color: "#97969B",
-  fontWeight: 300,
-  paddingBottom: 5
-};
-
-const titleStyle = {
-  paddingBottom: 5,
-  fontWeight: 500,
-  fontSize: "1.6em"
-};
-
-const dataStyle = {
-  fontSize: "1.2em",
-  fontWeight: 500
-};
+import styles from "./list.module.css";
 
 export default function List({ venue, sort }: { venue: string; sort: Sort }) {
   const { loading, error, data } = useQuery(GET_BEERS, {
@@ -67,7 +44,7 @@ export default function List({ venue, sort }: { venue: string; sort: Sort }) {
                 <Image width={75} height={75} src={beer.labelImageUrl || "/badge-beer-default.png"} alt={beer.beer} />
               </Col>
               <Col xs={18} sm={14} md={10} style={{ lineHeight: 1.1 }}>
-                <div style={titleStyle}>
+                <div className={styles.title} style={{ fontSize: "1.5em", fontWeight: 500 }}>
                   {beer.id ? (
                     <a href={`https://untappd.com/beer/${beer.id}`} target="_blank" rel="nofollow noreferrer">
                       {beer.beer}
@@ -76,15 +53,17 @@ export default function List({ venue, sort }: { venue: string; sort: Sort }) {
                     beer.beer
                   )}
                 </div>
-                <div style={{ ...titleStyle, ...{ fontSize: "1.2em" } }}>{beer.brewery}</div>
-                <div style={slimText}>
+                <div className={styles.title} style={{ fontSize: "1.3em" }}>
+                  {beer.brewery}
+                </div>
+                <div className={styles.slim} style={{ fontSize: "0.9em" }}>
                   <div>{beer.style}</div>
                 </div>
               </Col>
               {beer.rating && (
-                <Col xs={8} md={4} style={cardStyle}>
-                  <div style={slimText}>Rating</div>
-                  <div style={dataStyle}>
+                <Col xs={8} md={4} className={styles.card}>
+                  <div className={styles.slim}>Rating</div>
+                  <div className={styles.data}>
                     {beer.rating?.toFixed(2)}
                     {beer.ratings && (
                       <small style={{ opacity: 0.5 }}>
@@ -95,15 +74,15 @@ export default function List({ venue, sort }: { venue: string; sort: Sort }) {
                 </Col>
               )}
               {beer.abv && (
-                <Col xs={8} md={4} style={cardStyle}>
-                  <div style={slimText}>ABV</div>
-                  <div style={dataStyle}>{beer.abv}%</div>
+                <Col xs={8} md={4} className={styles.card}>
+                  <div className={styles.slim}>ABV</div>
+                  <div className={styles.data}>{beer.abv}%</div>
                 </Col>
               )}
               {beer.ibu && (
-                <Col xs={8} md={4} style={cardStyle}>
-                  <div style={slimText}>IBU</div>
-                  <div style={dataStyle}>{beer.ibu}</div>
+                <Col xs={8} md={4} className={styles.card}>
+                  <div className={styles.slim}>IBU</div>
+                  <div className={styles.data}>{beer.ibu}</div>
                 </Col>
               )}
             </Row>
