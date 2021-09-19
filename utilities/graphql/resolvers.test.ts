fdc812a5fc496dfa3e20948ce70acb3a60b41bc2
Kyle Karpack <kylekarpack@gmail.com>
2021-09-19 13:34:24 -0700

    Add resolver tests

---
+++ utilities/graphql/resolvers.test.ts
@@ -0,0 +1,26 @@
+import { resolvers } from "./resolvers";
+import axios from "axios";
+
+jest.mock("axios");
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+mockedAxios.get.mockResolvedValue({
+  data: [
+    {
+      beer: "Brewery: Test Beer"
+    }
+  ]
+});
+
+describe("resolvers", () => {
+  it("loads resolvers", async () => {
+    const result = resolvers.Query.beers(null as never, { venue: "chucks" });
+    await expect(result).resolves.toBeTruthy();
+    expect(mockedAxios.get).toHaveBeenCalledTimes(1);
+  });
+
+  it("fails when resolver does not exist", async () => {
+    const result = resolvers.Query.beers(null as never, { venue: "VenueDoesNotExist" });
+    await expect(result).rejects.toBeTruthy();
+		expect(mockedAxios.get).toHaveBeenCalledTimes(1);
+  });
+});
