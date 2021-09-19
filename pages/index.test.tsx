7899c6f56238c4824d7a23a0f5023bae3a4db1b5
Kyle Karpack <kylekarpack@gmail.com>
2021-09-18 17:26:55 -0700

    Test changes

--- pages/index.test.tsx
+++ pages/index.test.tsx
@@ -1,7 +1,7 @@
+import { MockedProvider } from "@apollo/react-testing";
 import { cleanup, render } from "@testing-library/react";
 import React from "react";
 import Index from "./index";
-import { MockedProvider } from "@apollo/react-testing";
 
 describe("index page", () => {
   afterEach(cleanup);
