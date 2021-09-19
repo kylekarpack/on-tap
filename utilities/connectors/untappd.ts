6beadb52e04c175d5fe6f3715fd72cf04a16c454
Kyle Karpack <kylekarpack@gmail.com>
2021-09-19 09:28:18 -0700

    Move creation logic

--- utilities/connectors/untappd.ts
+++ utilities/connectors/untappd.ts
@@ -3,9 +3,9 @@ import axios from "axios";
 import { Beer, AlgoliaBeer } from "utilities/types";
 
 export default class Untappd {
-  async getBeer(beer: Beer): Promise<Partial<Beer>> {
+  async getBeer(beer: Beer): Promise<Beer> {
     let searchBeer = beer.beer?.replace(/ (ipa|stout|porter|sour|hazy|cider|tripel)$/gi, "");
-		searchBeer = searchBeer.split(" - ")[0];
+    searchBeer = searchBeer.split(" - ")[0];
     const search = await axios.post(
       `https://${process.env.NEXT_PUBLIC_ALGOLIA_SERVER}.algolia.net/1/indexes/beer/query?x-algolia-agent=Algolia%20for%20vanilla%20JavaScript%203.24.8&x-algolia-application-id=${process.env.NEXT_PUBLIC_ALGOLIA_APPLICATION_ID}&x-algolia-api-key=${process.env.NEXT_PUBLIC_ALGOLIA_API_KEY}`,
       {
@@ -28,20 +28,6 @@ export default class Untappd {
 
     const { data } = search;
     const result: AlgoliaBeer = data.hits[0];
-
-    if (!result) {
-      return {};
-    }
-
-    return {
-      id: result.bid,
-      beer: result.beer_name,
-      brewery: result.brewery_name,
-      style: result.type_name,
-      rating: Math.round(result.rating_score * 100) / 100 || null,
-      ratings: Number(result.rating_count) || null,
-      abv: result.beer_abv,
-      labelImageUrl: result.beer_label
-    };
+    return new Beer().fromAlgoliaBeer(result);
   }
 }
--- utilities/types/beer.ts
+++ utilities/types/beer.ts
@@ -1,6 +1,30 @@
+import { AlgoliaBeer } from ".";
+
 export class Beer {
-  constructor(source: Partial<Beer>) {
-    Object.assign(this, source);
+  constructor(source?: Partial<Beer>) {
+		if (source != null) {
+			Object.assign(this, source);
+		}
+  }
+
+  public fromAlgoliaBeer(source: AlgoliaBeer) {
+
+		if (source == null) {
+			return new Beer();
+		}
+
+    const beer: Partial<Beer> = {
+      id: source.bid,
+      beer: source.beer_name,
+      brewery: source.brewery_name,
+      style: source.type_name,
+      rating: Math.round(source.rating_score * 100) / 100 || null,
+      ratings: Number(source.rating_count) || null,
+      abv: source.beer_abv,
+      labelImageUrl: source.beer_label
+    };
+
+    return new Beer(beer);
   }
 
   public augment?(beer: Partial<Beer>) {
