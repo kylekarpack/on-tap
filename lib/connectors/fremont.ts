09661c1b2f0b4fc272bce6bc428e5623ebb57255
Kyle Karpack <kylekarpack@gmail.com>
2022-06-12 17:16:08 -0700

    Add Fremont shell

---
+++ lib/connectors/fremont.ts
@@ -0,0 +1,14 @@
+import ConnectorBase from "./connectorBase";
+
+/**
+ * The connector for Fremont Brewing
+ */
+export default class Fremont extends ConnectorBase {
+  /**
+   * Build the connector
+   */
+  constructor() {
+    super();
+    throw new Error("Not implemented");
+  }
+}
