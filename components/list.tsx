138b9ca71e15fd47257b4dc81705b705016ba3ce
Kyle Karpack <kylekarpack@gmail.com>
2023-12-29 15:36:46 -0800

    Card refactor

--- components/list.tsx
+++ components/list.tsx
@@ -1,18 +1,16 @@
 import { useQuery } from "@apollo/client";
-import { Card, CardBody, CircularProgress } from "@nextui-org/react";
-import Image from "next/image";
+import { CircularProgress } from "@nextui-org/react";
 import { FunctionComponent } from "react";
 import { GET_BEERS } from "lib/queries";
-import { Beer, Sort, Venue } from "lib/types";
+import { Beer as BeerType, Sort, Venue } from "lib/types";
 import { sortTable } from "lib/utils";
-import BeerLink from "./beerLink";
-import { CompactNumber, Percentage } from "./number";
+import Beer from "./Beer";
 
 /**
  * Render a list of beers
  */
 const BeerList: FunctionComponent<{ venue: Venue; sort: Sort }> = ({ venue, sort }) => {
-  const { loading, error, data } = useQuery<{ beers: Beer[] }>(GET_BEERS, {
+  const { loading, error, data } = useQuery<{ beers: BeerType[] }>(GET_BEERS, {
     variables: {
       venue: venue.value,
       params: {
@@ -38,67 +36,11 @@ const BeerList: FunctionComponent<{ venue: Venue; sort: Sort }> = ({ venue, sort
     );
   }
 
-  const listData: Beer[] = sortTable(sort, data?.beers) ?? [];
+  const listData: BeerType[] = sortTable(sort, data?.beers) ?? [];
   return (
     <div data-testid="list">
       {listData.map((beer) => (
-        <Card isBlurred isPressable shadow="none" radius="none" className="border-b-1 border-gray-700" key={beer.guid}>
-          <CardBody className="px-4 py-6">
-            <div className="grid grid-cols-6 md:grid-cols-12 gap-6 md:gap-12 items-center justify-center">
-              <div className="grid grid-cols-5 col-span-7 gap-4 md:gap-12 items-center md:items-start">
-                <div className="relative col-span-1">
-                  <Image
-                    width={100}
-                    height={100}
-                    alt="Beer Label"
-                    className="object-cover rounded-lg shadow-md"
-                    src={beer.labelImageUrl ?? "/badge-beer-default.png"}
-                  />
-                </div>
-                <div className="col-span-4">
-                  <div className="text-md sm:text-xl font-semibold">
-                    <BeerLink beer={beer} />
-                  </div>
-                  <div className="text-gray-100 text-sm">
-                    {beer.brewery} {beer.style ? "-" : ""} {beer.style}
-                  </div>
-                </div>
-              </div>
-
-              <div className="col-span-5">
-                <div className="grid grid-cols-3 gap-4 md:gap-12">
-                  {beer.rating ? (
-                    <div className="col-span-1">
-                      <div className="font-light text-sm text-gray-300">Rating</div>
-                      <div className="text-xl">
-                        {beer.rating?.toFixed(2)}
-                        {beer.ratings && (
-                          <small className="text-gray-400 text-xs">
-                            &nbsp; (<CompactNumber value={beer.ratings} />)
-                          </small>
-                        )}
-                      </div>
-                    </div>
-                  ) : null}
-                  {beer.abv && beer.abv !== 0 ? (
-                    <div className="col-span-1">
-                      <div className="font-light text-sm text-gray-300">ABV</div>
-                      <div className="text-xl">
-                        <Percentage value={beer.abv} />
-                      </div>
-                    </div>
-                  ) : null}
-                  {beer.ibu && beer.ibu !== 0 ? (
-                    <div className="col-span-1">
-                      <div className="font-light text-sm text-gray-300">IBU</div>
-                      <div className="text-xl">{beer.ibu}</div>
-                    </div>
-                  ) : null}
-                </div>
-              </div>
-            </div>
-          </CardBody>
-        </Card>
+        <Beer beer={beer} key={beer.guid} />
       ))}
     </div>
   );
